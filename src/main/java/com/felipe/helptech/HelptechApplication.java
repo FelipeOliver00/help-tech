4e2d5401ae58e220defff2e3764d3d4e6221ae21
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-10-31 13:13:32 -0300

    Initial Project

---
+++ src/main/java/com/felipe/helptech/HelptechApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.helptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelptechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelptechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/helptech/HelptechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.helptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelptechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
