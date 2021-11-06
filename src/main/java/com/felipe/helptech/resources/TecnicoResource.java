b37543456e90036757f4adf3907e04e22e48dd10
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-11-06 12:16:27 -0300

    DTO pattern for technician

--- src/main/java/com/felipe/helptech/resources/TecnicoResource.java
+++ src/main/java/com/felipe/helptech/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.helptech.domain.Tecnico;
+import com.felipe.helptech.domain.dtos.TecnicoDTO;
 import com.felipe.helptech.services.TecnicoService;
 
 @RestController
@@ -20,9 +21,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = this.service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
 }
