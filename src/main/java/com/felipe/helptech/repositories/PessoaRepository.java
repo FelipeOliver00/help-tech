da1fd074b815d5e27038e5ac21a908a5582493d0
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-11-07 13:22:42 -0300

    exception of cpf and email already registered

--- src/main/java/com/felipe/helptech/repositories/PessoaRepository.java
+++ src/main/java/com/felipe/helptech/repositories/PessoaRepository.java
@@ -1,9 +1,15 @@
 package com.felipe.helptech.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.felipe.helptech.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-
+	
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
+	
 }
