90680fb99448f65ebfd5d9680af1fc09f8edaf5d
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-11-07 12:53:05 -0300

    endpoint 'create' for Tecnico

--- src/main/java/com/felipe/helptech/services/TecnicoService.java
+++ src/main/java/com/felipe/helptech/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipe.helptech.domain.Tecnico;
+import com.felipe.helptech.domain.dtos.TecnicoDTO;
 import com.felipe.helptech.repositories.TecnicoRepository;
 import com.felipe.helptech.services.exceptions.ObjectnotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 	
 }
