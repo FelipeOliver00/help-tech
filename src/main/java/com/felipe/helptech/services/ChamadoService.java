2c44555f029c82d91b97115deb4e693da1831a91
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-11-12 13:16:56 -0300

    create for Chamado

--- src/main/java/com/felipe/helptech/services/ChamadoService.java
+++ src/main/java/com/felipe/helptech/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.felipe.helptech.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipe.helptech.domain.Chamado;
+import com.felipe.helptech.domain.Cliente;
+import com.felipe.helptech.domain.Tecnico;
+import com.felipe.helptech.domain.dtos.ChamadoDTO;
+import com.felipe.helptech.domain.enums.Prioridade;
+import com.felipe.helptech.domain.enums.Status;
 import com.felipe.helptech.repositories.ChamadoRepository;
 import com.felipe.helptech.services.exceptions.ObjectnotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id){
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,6 +34,29 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	// Usando para criar e atualizar
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 	
 }
