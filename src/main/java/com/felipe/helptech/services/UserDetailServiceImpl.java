52aed7e848d66216b88728b0103785656b16545b
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-11-13 16:34:56 -0300

    full authentication JWT

---
+++ src/main/java/com/felipe/helptech/services/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.felipe.helptech.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.felipe.helptech.domain.Pessoa;
+import com.felipe.helptech.repositories.PessoaRepository;
+import com.felipe.helptech.security.UserSS;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private PessoaRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if(user.isPresent()){
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+
+}
